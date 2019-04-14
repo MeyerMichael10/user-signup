806e4198d7fe204a32e20358675ec1f91fa6e5d6
Mike Meyer <myke428@gmail.com>
2019-04-14 00:28:43 -0500

    added signup form, started validation

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 import os
-import jinja2
+import cgi
 
-template_dir = os.path.join(os.path.dirname(__file__),
-    'templates')
-jinja_env = jinja2.environment(
-    loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
-app = flask(__name__)
+
+
+app = Flask(__name__)
 app.config['debug'] = True
+
+@app.route("/")
+def index():
+    return render_template('sign-up.html')
+
+@app.route("/sign_in", methods=["POST"])
+def sign_in():
+    username = request.form["username"]
+    password = request.form["password"]
+    verify_password = request.form["verify_password"]
+    email = request.form["email"]
+
+    username_error = ''
+    password_error = ''
+    verify_error = ''
+    email_error = ''
+#TODO: validate inputs
+    if len(username) < 3 or len(username) > 20:
+        username = ''
+        username_error = 'Username must be between 3 and 20 characters long'
+    else:
+        if " " in username:
+            username = ''
+            username_error = 'Username cannot contain spaces'
+    
+
+    if not username_error and not password_error and not verify_error and not email_error:
+        return redirect('/welcome')
+    else return render_template('sign-in.html', username_error = username_error,
+    password_error = password_error, verify_error = verify_error, email_error = email_error,
+    username = username, password = password, verify_password = verify_password, email = email)
+#TODO: add welcome page
+
+
+
+app.run()
+    
